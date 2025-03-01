cdc2ed3a825eac9275683b4ebe12aea5cba7acf2
shimura <shimura.shun.nagoyaml@gmail.com>
2025-03-01 11:32:02 +0900

    predict done

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+''' Predict unknown object positions from images up to the time '''
 import os,sys,shutil
 
 import logging, argparse
@@ -13,8 +14,6 @@ from sliced_multi_object_tracker.detector import *
 from sliced_multi_object_tracker.filter import *
 from sliced_multi_object_tracker.tracker import *
 
-
-# parser
 def parse_hyp(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=Path, default=Path('.')/'data', help="Path to input folder of images.")
@@ -30,10 +29,8 @@ def parse_hyp(known=False):
     parser.add_argument("--q-var", type=float, default=1.0, help="Elements of Q on estimation (default 100.0)")
     parser.add_argument("--r-var", type=float, default=1.0, help="Elements of R on estimation (default 100.0)")
     parser.add_argument("--log", type=Path, default=Path('logs/default.log'), help="Path to files where log is written.")
-    parser.add_argument("--gt-detect", action="store_true", default=False, help="Whether use ground_truth of detection or not")
-    parser.add_argument("--gt-reid", action="store_true", default=False, help="Whether use ground_truth of reidentification or not")
     return parser.parse_known_args()[0] if known else parser.parse_args()
-    
+
 # logger
 def determine_log(name, log:Path=Path(".")/'out.log') -> logging.Logger:
     logger = logging.getLogger(name)
@@ -47,12 +44,12 @@ def determine_log(name, log:Path=Path(".")/'out.log') -> logging.Logger:
 
 if __name__ == '__main__':
     
-    # parser hyper-parameters
+    # Parse hyper-parameters
     hyp = parse_hyp()
-    # logger
+    # Logger
     logger = determine_log('mylogger', hyp.log)
     
-    # make directories
+    # Make directories 
     output_folder = hyp.output
     detection_folder = output_folder / 'detection'
     reid_folder = output_folder / 'reid'
@@ -61,10 +58,10 @@ if __name__ == '__main__':
     create_or_clear_directory(reid_folder)
     create_or_clear_directory(estimation_folder)
     
-    # for yolo detection
+    # For yolo detection
     detection_tmp_folder = detection_folder / 'tmp'
-    
-    # constract
+
+    # Constract     
     data_loader = DataLoader(
         ns=hyp.ns, 
         nf=hyp.nf,
@@ -90,76 +87,58 @@ if __name__ == '__main__':
         model=sliced_model,
         bbox_model=bbox_model,
         mode=hyp.mode
-    )
+    )    
     
-    ### Main process ###
+    ### Main Process ###
     
     for t in tqdm(range(hyp.nf)):
         
         logger.critical(enclosed_by_dash_lines(f"Step {str(t).zfill(3)} starts", factor=100))
 
-        ## Detection ## 
+        ## Detection ##
         
-        # use ground truth
-        if hyp.gt_detect:
-            detected_bboxes: Dict[int, np.ndarray] = data_loader.load_detection_labels(time=t)
+        image_paths: List[Path] = data_loader.load_image_paths(time=t)
+        create_or_clear_directory(detection_tmp_folder)
+        for ip in image_paths:
+            shutil.copy(ip, detection_tmp_folder)
             
-        # use detector and images
-        else:
-            # copy images to tmp_folder
-            image_paths: List[Path] = data_loader.load_image_paths(time=t)
-            create_or_clear_directory(detection_tmp_folder)
-            for ip in image_paths:
-                shutil.copy(ip, detection_tmp_folder)
-                
-            # save results
-            detection_results: List[Results] = detector(
+        # save results
+        detection_results: List[Results] = detector(
                 source=detection_tmp_folder,
                 conf=0.1,
                 imgsz=640,
                 stream=False,
                 verbose=False,
                 device=1
-            )
-            detected_bboxes: Dict[int, np.ndarray] = {}
-            for s, r in enumerate(detection_results):
-                r.save(dir=detection_folder/'annotated') # no need of preliminary makedir
-                boxes = r.boxes.xywhn.detach().cpu().numpy().astype(float)
-                confs = r.boxes.conf.detach().cpu().numpy().astype(float)
-                idx = non_maximum_suppression(boxes, confs, iou_threshold=0.5)
-                if not Path(detection_folder / 'text').exists():
-                    os.makedirs(detection_folder / 'text')
-                np.savetxt(
-                    detection_folder/'text'/f"t{str(t).zfill(3)}s{str(s).zfill(3)}.txt",
-                    np.hstack((boxes[idx], confs[idx].reshape(-1, 1))), 
-                    fmt="%.8f"
-                    )
-                detected_bboxes[s] = boxes[idx]
-                
-        ## Tracking ## 
-        
-        # use ground truth
-        if hyp.gt_reid:
-            gt_bboxes = data_loader.load_reidentification_labels(time=t)
-            tracker.gt_step(gt_bboxes)
+        )
+        detected_bboxes: Dict[int, np.ndarray] = {}
+        for s, r in enumerate(detection_results):
+            r.save(dir=detection_folder/'annotated') # no need of preliminary makedir
+            boxes = r.boxes.xywhn.detach().cpu().numpy().astype(float)
+            confs = r.boxes.conf.detach().cpu().numpy().astype(float)
+            idx = non_maximum_suppression(boxes, confs, iou_threshold=0.5)
+            if not Path(detection_folder / 'text').exists():
+                os.makedirs(detection_folder / 'text')
+            np.savetxt(
+                detection_folder/'text'/f"t{str(t).zfill(3)}s{str(s).zfill(3)}.txt",
+                np.hstack((boxes[idx], confs[idx].reshape(-1, 1))), 
+                fmt="%.8f"
+                )
+            detected_bboxes[s] = boxes[idx]
             
-        # use reidentifier
-        else:
-            # convert array-like to Bbox
-            sliced_bboxes = {
-                s: [
-                    Bbox(b, t, s) for b in detected_bboxes[s]
-                ] for s in range(hyp.ns)
-            }
-            tracker.step(sliced_bboxes)
+        ## Tracking
+        
+        sliced_bboxes = {
+            s: [
+                Bbox(b, t, s) for b in detected_bboxes[s]
+            ] for s in range(hyp.ns)
+        }
+        tracker.step(sliced_bboxes)
             
         logger.info(f"Tracking {len(tracker.single_trackers)} objects.")
-            
+        
     all_bboxes:List[Set[Bbox]] = tracker.all_bboxes
     all_predictions:List[Dict[int, np.ndarray]] = tracker.all_predictions
     all_filterings:List[Dict[int, List[np.ndarray]]] = tracker.all_filterings
-    # save_prediction(all_predictions, hyp.nf, dir=estimation_folder)
-    # save_meanIoU(all_predictions, hyp.nf, data_loader, dir=estimation_folder)
-    # save_motmetric(all_bboxes, hyp.nf, data_loader, dir=reid_folder)
-    # save_detection_map(all_bboxes, hyp.ns, hyp.nf, dir=reid_folder)
-    # save_posterior(all_predictions, all_filterings, hyp.nf, data_loader, dir=estimation_folder)
+    save_prediction(all_predictions, hyp.nf, dir=estimation_folder)
+    save_detection_map(all_bboxes, hyp.ns, hyp.nf, dir=reid_folder)
